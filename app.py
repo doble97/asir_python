79f10715affeecc9a93d19ddd0eaa57d44ad9897
Jorge Gonzalez <jorgegonzalez97d@gmail.com>
2022-03-23 14:00:13 +0100

    Subida desde clase

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import os
 from pickle import TRUE
 
-
+limpiar_pantalla=lambda: os.system('cls' if os.name=='nt' else 'clear')
 class Agenda:
     def __init__(self):
         self.dato=list()
@@ -19,25 +20,37 @@ class Agenda:
         for x in range(7):
             print(menu[x][0])
         
-    def add_contacto(self):
-        pass
-    def lista_contacto(self):
-        pass
+    def add_contacto(self, contacto):
+        self.dato.append(contacto)
+    def lista_contacto(self)->None:
+        if not self.agenda_vacia():
+            for x in self.dato:
+                print(f'***\n{x["nombre"]}\n{x["telefono"]}\n{x["email"]}\n***')
+        else:
+            print('No tienes contactos todavía')
     def buscar_contacto(self):
         pass
     def editar_contacto(self):
         pass
     def eliminar_contacto(self):
         pass
+    def agenda_vacia(self)->bool:
+        return len(self.dato)==0
     
 agenda = Agenda()
-agenda.menu()
 while True:
-    opcion = 1
+    agenda.menu()
+    opcion = int(input('Opcion 1-6: '))
+    limpiar_pantalla()
     if opcion==1:
+        contacto= dict()
+        contacto['nombre']=input('Nombre del contacto: ')
+        contacto['telefono']=int(input('Telefono del contacto: '))
+        contacto['email'] = input('Correo del contacto: ')
+        agenda.add_contacto(contacto)
         pass
     elif opcion==2:
-        pass
+        agenda.lista_contacto()
     elif opcion==3:
         pass
     elif opcion==4:
